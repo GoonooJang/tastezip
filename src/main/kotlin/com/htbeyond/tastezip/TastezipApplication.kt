bc2b075e14de03d7bedd7099d2f03861214b9f68
장군우 <gonoo0731@jang-gun-uui-MacBookPro.local>
2023-01-02 19:29:31 +0900

    Initial commit

---
+++ src/main/kotlin/com/htbeyond/tastezip/TastezipApplication.kt
@@ -0,0 +1,11 @@
+package com.htbeyond.tastezip
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TastezipApplication
+
+fun main(args: Array<String>) {
+    runApplication<TastezipApplication>(*args)
+}
---
+++ src/test/kotlin/com/htbeyond/tastezip/TastezipApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.htbeyond.tastezip
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TastezipApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
